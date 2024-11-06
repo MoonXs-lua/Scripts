fcbc45a5855f29c8f7ddb6b7652671c30e7c8781
SevenXs-script <sz937@proton.me>
2024-11-06 12:19:05 -0300

    Update Natural-disaster.lua

--- Scripts/Natural-disaster.lua
+++ Scripts/Natural-disaster.lua
@@ -1,1 +1,11 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+local Window = Fluent:CreateWindow({
+    Title = "Seven hub",
+    SubTitle = "by SevenXs",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
